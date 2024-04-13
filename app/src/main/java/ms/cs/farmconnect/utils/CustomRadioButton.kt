f03b8fef55c1b55978f7b091cb48b300e9700c7d
adityadeval <adityadeval27@gmail.com>
2024-04-13 16:59:41 -0400

    + Created User Profile Activity and Layout "(36. 37.)"

---
+++ app/src/main/java/ms/cs/farmconnect/utils/CustomRadioButton.kt
@@ -0,0 +1,22 @@
+package ms.cs.farmconnect.utils
+
+import android.content.Context
+import android.graphics.Typeface
+import android.util.AttributeSet
+import androidx.appcompat.widget.AppCompatRadioButton
+
+class CustomRadioButton(context: Context, attrs: AttributeSet) :
+    AppCompatRadioButton(context, attrs) {
+
+    init {
+        applyFont()
+    }
+
+    private fun applyFont() {
+
+        val typeface: Typeface =
+            Typeface.createFromAsset(context.assets, "Montserrat-Bold.ttf")
+        setTypeface(typeface)
+    }
+
+}

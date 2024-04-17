8951114bcd5eb37831dfc7c2e4672f217b6af3b3
adityadeval <adityadeval27@gmail.com>
2024-04-17 19:59:07 -0400

    + Added a Settings vector image on the Dashboard fragment + Implemented the Settings Activity and it's layout."(56. 57.)"

--- app/src/main/java/ms/cs/farmconnect/ui/fragments/DashboardFragment.kt
+++ app/src/main/java/ms/cs/farmconnect/ui/fragments/DashboardFragment.kt
@@ -1,13 +1,19 @@
 package ms.cs.farmconnect.ui.fragments
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
+import ms.cs.farmconnect.R
 //import androidx.lifecycle.ViewModelProvider
 import ms.cs.farmconnect.databinding.FragmentDashboardBinding
+import ms.cs.farmconnect.ui.activities.SettingsActivity
 
 class DashboardFragment : Fragment() {
 
@@ -16,6 +22,11 @@ class DashboardFragment : Fragment() {
     // This property is only valid between onCreateView and
     // onDestroyView.
     private val binding get() = _binding!!
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setHasOptionsMenu(true)
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -36,6 +47,25 @@ class DashboardFragment : Fragment() {
 //            textView.text = "This is dashboard Fragment"
 //        }
         return root
+    }
+
+    // Below function inflates the dashboard menu.
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.dashboard_menu, menu)
+        super.onCreateOptionsMenu(menu, inflater)
+    }
+
+    // Below function handles click events for the Dashboard menu.
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        val id = item.itemId
+
+        when (id) {
+            R.id.action_settings -> {
+                startActivity(Intent(activity, SettingsActivity::class.java))
+                return true
+            }
+        }
+        return super.onOptionsItemSelected(item)
     }
 
     override fun onDestroyView() {

ad44572dbf6ca5d1677ef04aec5805f9d0e9fb53
adityadeval <adityadeval27@gmail.com>
2024-04-18 16:32:50 -0400

    + Added click listener for the add product image ImageView. + Enabled adding product image in the Add Product Activity when clicked on the add product image ImageView"(64.)"

--- app/src/main/java/ms/cs/farmconnect/ui/activities/AddProductActivity.kt
+++ app/src/main/java/ms/cs/farmconnect/ui/activities/AddProductActivity.kt
@@ -1,21 +1,41 @@
 package ms.cs.farmconnect.ui.activities
 
+import android.Manifest
+import android.app.Activity
+import android.content.Intent
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.ImageView
+import android.widget.Toast
 import androidx.appcompat.widget.Toolbar
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import ms.cs.farmconnect.R
+import ms.cs.farmconnect.utils.Constants
+import ms.cs.farmconnect.utils.GlideLoader
+import java.io.IOException
 
-class AddProductActivity : AppCompatActivity() {
+class AddProductActivity : BaseActivity(), View.OnClickListener {
 
     private lateinit var toolbar_add_product_activity : Toolbar
+    private lateinit var iv_add_update_product : ImageView
+    private lateinit var iv_product_image : ImageView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_product)
 
         toolbar_add_product_activity  = findViewById(R.id.toolbar_add_product_activity )
+        iv_add_update_product = findViewById(R.id.iv_add_update_product)
+        iv_product_image = findViewById(R.id.iv_product_image)
 
         setupActionBar()
+
+        // Set on Click listener for the little add_image icon that appears just beside the product's image.
+        iv_add_update_product.setOnClickListener(this)
     }
 
     private fun setupActionBar() {
@@ -29,6 +49,102 @@ class AddProductActivity : AppCompatActivity() {
         }
 
         toolbar_add_product_activity.setNavigationOnClickListener { onBackPressed() }
+    }
+
+    override fun onClick(v: View?) {
+        if (v != null) {
+            when (v.id) {
+
+                R.id.iv_add_update_product -> {
+                    // Check if External storage permission has been granted by user already. If not, ask for it.
+                    // We already ask for this permission during profile creation for the profile photo.
+                    // But user might choose not to add profile image, in which case, the app won't have asked for the storage permissions.
+                    if (ContextCompat.checkSelfPermission(
+                            this,
+                            Manifest.permission.READ_EXTERNAL_STORAGE
+                        )
+                        == PackageManager.PERMISSION_GRANTED
+                    ) {
+                        // Above if is true, which means app has storage permission.
+                        // In this case call the ImageChooser function to allow user to select product image from the storage.
+                        // This function in turn calls the StartActivityForResult(), which would allow the user to pick an image from the storage
+                        // in the pop up dialog. Once done, Android will call the onActivityResult() method.
+                        Constants.showImageChooser(this@AddProductActivity)
+                    } else {
+                        // Request storage permissions.
+                        // Once they are granted or denied in the popup dialog, the result is handled by the
+                        // onRequestPermissionsResult function.
+                        ActivityCompat.requestPermissions(
+                            this,
+                            arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE),
+                            Constants.READ_STORAGE_PERMISSION_CODE
+                        )
+                    }
+                }
+
+
+            }
+        }
+    }
+
+    // This function executes, once the popup dialog for requesting permissions from user is shown and
+    // the user clicks on some option.
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+
+        // If the result is for the read storage permission.
+        if (requestCode == Constants.READ_STORAGE_PERMISSION_CODE) {
+            //If permission is granted by the user.
+            if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                Constants.showImageChooser(this)
+            } else {
+                //Displaying toast if permission is not granted.
+                Toast.makeText(
+                    this,
+                    resources.getString(R.string.read_storage_permission_denied),
+                    Toast.LENGTH_LONG
+                ).show()
+            }
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (resultCode == Activity.RESULT_OK) {
+            if (requestCode == Constants.PICK_IMAGE_REQUEST_CODE) {
+                if (data != null) {
+                    iv_add_update_product.setImageDrawable(
+                        ContextCompat.getDrawable(
+                            this@AddProductActivity,
+                            R.drawable.ic_vector_edit
+                        )
+                    )
+
+                    // data parameter of onActivityResult will contain data returned by the popup screen for selecting image.
+                    // data.data would be the uri of the locally stored image that the user selected.
+                    // Storing this uri in SelectedImageFileUri
+                    val SelectedImageFileUri = data.data!!
+
+                    try {
+                        // Load the product image in the ImageView.
+                        GlideLoader(this@AddProductActivity).loadUserPicture(
+                            SelectedImageFileUri!!,
+                            iv_product_image
+                        )
+                    } catch (e: IOException) {
+                        e.printStackTrace()
+                    }
+                }
+            }
+        } else if (resultCode == Activity.RESULT_CANCELED) {
+            Log.e("Request Cancelled", "Product image selection cancelled")
+        }
+
     }
 
 }

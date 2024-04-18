dc277ba7ea0066b4fd81eaf02b4c2de6b5f339dc
adityadeval <adityadeval27@gmail.com>
2024-04-18 12:25:51 -0400

    + Created a menu for the ProductsFragment + Created Add Product Activity and enabled launching it from the Products Fragment's menu. "(61. 62.)"

---
+++ app/src/main/java/ms/cs/farmconnect/ui/activities/AddProductActivity.kt
@@ -0,0 +1,12 @@
+package ms.cs.farmconnect.ui.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import ms.cs.farmconnect.R
+
+class AddProductActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_product)
+    }
+}
--- app/src/main/java/ms/cs/farmconnect/ui/fragments/ProductsFragment.kt
+++ app/src/main/java/ms/cs/farmconnect/ui/fragments/ProductsFragment.kt
@@ -1,13 +1,19 @@
 package ms.cs.farmconnect.ui.fragments
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
+import ms.cs.farmconnect.R
 //import androidx.lifecycle.ViewModelProvider
 import ms.cs.farmconnect.databinding.FragmentProductsBinding
+import ms.cs.farmconnect.ui.activities.AddProductActivity
 
 class ProductsFragment : Fragment() {
 
@@ -16,6 +22,12 @@ class ProductsFragment : Fragment() {
     // This property is only valid between onCreateView and
     // onDestroyView.
     private val binding get() = _binding!!
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        // If we want to use the option menu in fragment we need to add it.
+        setHasOptionsMenu(true)
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -41,4 +53,22 @@ class ProductsFragment : Fragment() {
         super.onDestroyView()
         _binding = null
     }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.add_product_menu, menu)
+        super.onCreateOptionsMenu(menu, inflater)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        val id = item.itemId
+
+        if (id == R.id.action_add_product) {
+
+            startActivity(Intent(activity, AddProductActivity::class.java))
+
+            return true
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
 }
